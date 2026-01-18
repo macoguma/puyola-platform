75645d982047e66e8df879071d7901fbf99e32ce
Oguma Austine <austineouma748@gmail.com>
2026-01-18 03:11:06 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,15 @@
-// login.js - Hard-coded passwords
 document.getElementById("loginBtn").addEventListener("click", () => {
   const password = document.getElementById("password").value.trim();
 
-  if(password === "Puyola@0032542025"){
-    // Admin login
+  if (password === "Puyola@0032542025") {
     window.location.href = "admin.html";
-  } else if(password === "Puyola@2025"){
-    // Member login
+  } else if (password === "Puyola@2025") {
     window.location.href = "member.html";
   } else {
     alert("Incorrect password!");
   }
 });
 
-// Show/Hide password
 document.getElementById("showPassword").addEventListener("change", () => {
   const pwd = document.getElementById("password");
   pwd.type = pwd.type === "password" ? "text" : "password";
